975be9d50ab6d0b375257ef1a2362db7139caa18
StarFenix <starfenix66@gmail.com>
2021-05-10 17:57:40 -0500

    La entidad Student, Enrollment y Course

--- ContosoUniversity/Models/Enrollment.cs
+++ ContosoUniversity/Models/Enrollment.cs
@@ -1,6 +1,21 @@
 ﻿namespace ContosoUniversity.Models
 {
+    public enum Grade
+    {
+        A, B, C, D, F
+    }
+
     public class Enrollment
     {
+        public int EnrollmentID { get; set; }
+        public int CourseID { get; set; }
+        public int StudentID { get; set; }
+        //La propiedad Grade es una enumeración.
+        //El signo de interrogación después de la declaración de tipo Grade
+        //indica que la propiedad Gradeacepta valores NULL.
+        public Grade? Grade { get; set; }
+
+        public virtual Course Course { get; set; }
+        public virtual Student Student { get; set; }
     }
 }
